b03a55b90770205d1c5e1f1f104b1015004e0617
muzammil-codeverse <muzammilcodeverse@gmail.com>
2024-07-28 16:25:38 +0500

    new calc app

---
+++ calculator/src/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val num1 = 10
+    val num2 = 5
+    val sum = num1 + num2
+    val sub = num1 - num2
+    val mult = num1 * num2
+    val divd = num1 / num2
+    println("Sum is $sum")
+    println("Sub is $sub")
+    println("Mult is $mult")
+    println("Divd is $divd")
+}
